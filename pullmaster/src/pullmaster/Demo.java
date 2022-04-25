3db69519d06abb6fc05258ba051b0a1f78fe0326
rahulrathod20 <104188895+rahulrathod20@users.noreply.github.com>
2022-04-25 13:14:04 +0530

    Update Demo.java

--- pullmaster/src/pullmaster/Demo.java
+++ pullmaster/src/pullmaster/Demo.java
@@ -2,6 +2,11 @@ package pullmaster;
 
 public class Demo {
 	
+	public void m1()
+	{
+		System.out.println("m1");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("nilesh branch");
