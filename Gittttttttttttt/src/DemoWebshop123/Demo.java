2abbe1aee06fd6ad3c70cee6fdd4fcc423250552
RAHUL <RAHUL@DESKTOP-NP6VH1C>
2022-04-28 00:00:32 +0530

    hhhhhh

--- Gittttttttttttt/src/DemoWebshop123/Demo.java
+++ Gittttttttttttt/src/DemoWebshop123/Demo.java
@@ -1,5 +1,6 @@
 package DemoWebshop123;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,10 +9,12 @@ public class Demo {
 	public static void main(String[] args) 
 	{
 		
-		System.setProperty("webdriver.chrome.driver", "D:\\\\chromedriverexefile\\\\chromedriver_win32 (2)\\\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\eclipce2019\\\\\\\\chromedriver_win32\\\\\\\\chromedriver 100\\\\\\\\chromedriver_win32 (2)\\\\\\\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		
 		driver.get("http://demowebshop.tricentis.com/");
+		
+		driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[1]/a")).click();
 		
 		
 	}
